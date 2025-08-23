65851e7b2f6b42710122c5d042f953ea0fed6a1e
jagdish gupta <varshneyjacky@gmail.com>
2025-08-23 23:54:14 +0530

    Navigation between components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { About } from './component/about/about';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//    {path:'', redirectTo: '/home',pathMatch: 'full'},
+ 
+   {path:'about', component:About},
+   {path:'', component:Home}
+];
