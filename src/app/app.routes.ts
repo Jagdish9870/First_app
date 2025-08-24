417394934543574d53ec0e82cc51c21d5412174e
jagdish gupta <varshneyjacky@gmail.com>
2025-08-24 21:14:09 +0530

    form validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,19 +5,24 @@ import { Home } from './home/home';
 export const routes: Routes = [
 //    {path:'', redirectTo: '/home',pathMatch: 'full'},
  
-   {path:'about/:id', component:About},
-   {path:'', component:Home},
-//    lazy loaading
-//    {
-//     path: '',
-//     loadComponent:() => import('./home/home').then((c)=>c.Home),
+//    {path:'about/:id', component:About},
+//    {path:'', component:Home},
+//    lazy loading
+   {
+    path: '',
+    loadComponent:() => import('./home/home').then((c)=>c.Home),
         
-//    },
-//    {
-//     path: 'about/:id',
-//     loadComponent:() => import('./component/about/about').then((c)=>c.About),
+   },
+   {
+    path: 'about/:id',
+    loadComponent:() => import('./about/about').then((c)=>c.About),
         
-//    }
+   },
+   {
+    path: 'admin',
+    loadComponent:() => import('./admin/admin').then((c)=>c.Admin),
+        
+   }
 
 
 ];
