a4e9bd1e6af7a97dd09ca61f3912c91ffa752a74
jagdish gupta <varshneyjacky@gmail.com>
2025-08-24 09:52:50 +0530

    about update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,23 @@
 import { Routes } from '@angular/router';
-import { About } from './component/about/about';
+import { About } from './about/about';
 import { Home } from './home/home';
 
 export const routes: Routes = [
 //    {path:'', redirectTo: '/home',pathMatch: 'full'},
  
    {path:'about/:id', component:About},
-   {path:'', component:Home}
+   {path:'', component:Home},
+//    lazy loaading
+//    {
+//     path: '',
+//     loadComponent:() => import('./home/home').then((c)=>c.Home),
+        
+//    },
+//    {
+//     path: 'about/:id',
+//     loadComponent:() => import('./component/about/about').then((c)=>c.About),
+        
+//    }
+
+
 ];
